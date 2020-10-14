f2042956ce9565fa16fc69c5a814831c649d09b5
dstrebel <strebeld@gmail.com>
2020-10-14 09:37:03 -0500

    add files

---
+++ aks.tf
@@ -0,0 +1,36 @@
+resource "azurerm_kubernetes_cluster" "privateaks" {
+  name                    = "private-aks"
+  location                = var.location
+  kubernetes_version      = "1.16.9"
+  resource_group_name     = var.kube_resource_group_name
+  dns_prefix              = "private-aks"
+  private_cluster_enabled = false
+
+  default_node_pool {
+    name           = "default"
+    node_count     = var.nodepool_nodes_count
+    vm_size        = var.nodepool_vm_size
+    vnet_subnet_id = module.kube_network.subnet_ids["aks-subnet"]
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    docker_bridge_cidr = var.network_docker_bridge_cidr
+    dns_service_ip     = var.network_dns_service_ip
+    network_plugin     = "kubenet"
+    outbound_type      = "userDefinedRouting"
+    service_cidr       = var.network_service_cidr
+  }
+
+  depends_on = [module.routtable]
+}
+
+# https://github.com/Azure/AKS/issues/1557
+resource "azurerm_role_assignment" "vmcontributor" {
+  role_definition_name = "Virtual Machine Contributor"
+  scope                = azurerm_resource_group.vnet.id
+  principal_id         = azurerm_kubernetes_cluster.privateaks.identity[0].principal_id
+}
---
+++ policy/aks-policy.rego
@@ -0,0 +1,30 @@
+package main
+
+has_field(obj, field) {
+    obj[field]
+}
+
+deny[msg] {
+    private := input.resource.azurerm_kubernetes_cluster[privateaks].private_cluster_enabled
+    private != "false"
+    msg = sprintf("Cluster `%v` is using Public API rather than Private API Cluster", [privateaks])
+}
+
+deny[msg] {
+    network_model = input.resource.azurerm_kubernetes_cluster[name]
+    has_field(network_model, "network_profile")
+    network_model.network_profile.network_plugin == "kubenet"
+    msg = sprintf("AKS Cluster `%v` is using Network Plugin Kubenet, which is not allowed. Please use azure CNI", [name])
+}
+
+#deny[msg] {
+#    rule := input.resource.aws_security_group_rule[name]
+#    rule.type == "ingress"
+#    contains(rule.cidr_blocks[_], "0.0.0.0/0") 
+#    msg = sprintf("ASG `%v` defines a fully open ingress", [name])
+#}
+
+#deny[msg] {
+#    count(resource_changes) > 0
+#    msg := sprintf("resource type %v is not pre-approved", [resource_changes[]])
+#}
